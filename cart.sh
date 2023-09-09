0022e0b9a4d4c1c1fc0d6813ed5b8c6ed22ef60e
Shyam <shyamneo24@gmail.com>
2023-09-09 21:08:55 +0530

    roboshop scripts

--- cart.sh
+++ cart.sh
@@ -9,6 +9,9 @@ curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 
+cd /app
+npm install
+
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
