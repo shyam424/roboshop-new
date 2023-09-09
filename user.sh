59371ab6b640b6448b100d8023a401b6d148b077
Shyam <shyamneo24@gmail.com>
2023-09-09 16:12:04 +0530

    roboshop scripts

---
+++ user.sh
@@ -0,0 +1,20 @@
+cp user.service /etc/systemd/system/user.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app || exit
+unzip /tmp/user.zip
+cd /app || exit
+npm install
+
+dnf install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+
+systemctl daemon-reload
+systemctl enable user
+systemctl restart user
