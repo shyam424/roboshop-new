dddf204e4b89713ab57e0153050a11509f48478a
Shyam <shyamneo24@gmail.com>
2023-09-09 20:23:54 +0530

    roboshop scripts

--- cart.sh
+++ cart.sh
@@ -6,7 +6,7 @@ dnf install nodejs -y
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app || exit
+cd /app
 unzip /tmp/cart.zip
 
 systemctl daemon-reload
